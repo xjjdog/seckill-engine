6a08a0ff9652e2d62ec727309bd7eb16d31d4929
lycying <lycying@gmail.com>
2019-08-31 13:12:54 +0800

    redis mock

--- sale-fast-core/src/main/java/com/github/xjjdog/sf/core/components/stock/StockServiceRedis.java
+++ sale-fast-core/src/main/java/com/github/xjjdog/sf/core/components/stock/StockServiceRedis.java
@@ -1,67 +1,96 @@
 package com.github.xjjdog.sf.core.components.stock;
 
-import com.github.xjjdog.sf.core.entity.RuntimeUnit;
 import com.github.xjjdog.sf.core.target.Target;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolAbstract;
 
-import java.util.HashMap;
+import java.util.Map;
 
 public class StockServiceRedis implements StockService {
-    /**
-     * 配置参数
-     */
-    HashMap<String, Target> targetHashMap = new HashMap<>();
-    /**
-     * 运行时实体
-     * 模拟分布式初始化锁
-     */
-    HashMap<String, RuntimeUnit> runtimeUnitHashMap = new HashMap<>();
+    JedisPoolAbstract pool;
+    JedisCluster cluster;
+    boolean isCluster;
+
+
+    final static String RedisKeyPrefix = "xjjdog:sf:";
+    final static String RuntimeCachePrefix = RedisKeyPrefix + ":rt:";
+
+    final static String HashKeyStock = "stock";
+
+    private int toInt(String str) {
+        if (null == str || "".equals(str)) {
+            return -1;
+        }
+        return Integer.parseInt(str);
+    }
 
     @Override
     public synchronized int stockNumber(Target target) {
         final String tagertID = target.getId();
+        final String key = RuntimeCachePrefix + tagertID;
+        Map<String, String> runtime = null;
+        if (isCluster) {
+            runtime = cluster.hgetAll(key);
+        } else {
+            try (Jedis jedis = pool.getResource()) {
+                runtime = jedis.hgetAll(key);
+            }
+        }
 
-        RuntimeUnit runtime = runtimeUnitHashMap.get(tagertID);
+        //check
         if (null == runtime) {
             return -1;
+        } else {
+            return toInt(runtime.get(HashKeyStock));
         }
-        return runtime.getStock();
     }
 
     @Override
     public synchronized boolean incStockNumber(Target target, int number) {
         final String tagertID = target.getId();
+        final String key = RuntimeCachePrefix + tagertID;
 
-        RuntimeUnit runtime = runtimeUnitHashMap.get(tagertID);
-        if (null == runtime) {
-            return false;
+        if (isCluster) {
+            cluster.hincrBy(key, HashKeyStock, number);
+        } else {
+            try (Jedis jedis = pool.getResource()) {
+                jedis.hincrBy(key, HashKeyStock, number);
+            }
+
         }
-
-        runtime.setStock(runtime.getStock() + number);
         return true;
     }
 
     @Override
     public synchronized boolean subStockNumber(Target target, int number) {
         final String tagertID = target.getId();
+        final String key = RuntimeCachePrefix + tagertID;
 
-        RuntimeUnit runtime = runtimeUnitHashMap.get(tagertID);
-        if (null == runtime) {
-            return false;
+        if (isCluster) {
+            cluster.hincrBy(key, HashKeyStock, number);
+        } else {
+            try (Jedis jedis = pool.getResource()) {
+                jedis.hincrBy(key, HashKeyStock, -number);
+            }
+
         }
-
-        runtime.setStock(runtime.getStock() - number);
         return true;
     }
 
     @Override
     public synchronized void fillStock(Target target) {
         final String tagertID = target.getId();
+        final String key = RuntimeCachePrefix + tagertID;
 
-        RuntimeUnit runtime = runtimeUnitHashMap.get(tagertID);
-        if (null == runtime) {
-            runtime = new RuntimeUnit();
-            runtime.setStock(target.getStock());
-            runtimeUnitHashMap.putIfAbsent(tagertID, runtime);
+        final String initStock = String.valueOf(target.getStock());
+
+        if (isCluster) {
+            cluster.hset(key, HashKeyStock, initStock);
+        } else {
+            try (Jedis jedis = pool.getResource()) {
+                jedis.hset(key, HashKeyStock, initStock);
+            }
         }
     }
 }
